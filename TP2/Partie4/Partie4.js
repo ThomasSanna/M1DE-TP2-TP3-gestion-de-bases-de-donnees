899013509703523e14aea95b29dad0a15f869957
Yohann2207 <yoyofbi2a@gmail.com>
2026-01-03 15:53:24 +0100

    TP2 finis

--- TP2/Partie4/Partie4.js
+++ TP2/Partie4/Partie4.js
@@ -30,3 +30,72 @@ db.jeux_de_donnees.aggregate([
     }
   }
 ]);
+
+// Durée moyenne entre la première et la dernière acquisition de fichiers par dataset (en jours)
+db.jeux_de_donnees.aggregate([
+  { $unwind: "$fichiers" },
+  {
+    $group: {
+      _id: "$id_jeu_de_donnees",
+      premiere_acquisition: { $min: { $toDate: "$fichiers.date_acquisition" } },
+      derniere_acquisition: { $max: { $toDate: "$fichiers.date_acquisition" } }
+    }
+  },
+  {
+    $project: {
+      duree_ms: { $subtract: ["$derniere_acquisition", "$premiere_acquisition"] }
+    }
+  },
+  {
+    $group: {
+      _id: null,
+      duree_moyenne_jours: { $avg: { $divide: ["$duree_ms", 1000 * 60 * 60 * 24] } },
+      duree_min_jours: { $min: { $divide: ["$duree_ms", 1000 * 60 * 60 * 24] } },
+      duree_max_jours: { $max: { $divide: ["$duree_ms", 1000 * 60 * 60 * 24] } }
+    }
+  }
+]);
+
+// Taux de fichiers image avec dimensions manquantes
+db.jeux_de_donnees.aggregate([
+  { $unwind: "$fichiers" },
+  { $match: { "fichiers.metadonnees_techniques.type": "image" } },
+  {
+    $project: {
+      largeur: "$fichiers.metadonnees_techniques.largeur",
+      hauteur: "$fichiers.metadonnees_techniques.hauteur"
+    }
+  },
+  {
+    $group: {
+      _id: null,
+      total_images: { $sum: 1 },
+      images_dimensions_manquantes: {
+        $sum: {
+          $cond: [
+            {
+              $or: [
+                { $eq: ["$largeur", null] },
+                { $eq: ["$hauteur", null] }
+              ]
+            },
+            1,
+            0
+          ]
+        }
+      }
+    }
+  },
+  {
+    $project: {
+      total_images: 1,
+      images_dimensions_manquantes: 1,
+      taux_dimensions_manquantes: {
+        $multiply: [
+          { $divide: ["$images_dimensions_manquantes", "$total_images"] },
+          100
+        ]
+      }
+    }
+  }
+]);
